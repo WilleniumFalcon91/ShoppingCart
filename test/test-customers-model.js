350dc56663fc1439c0b99778f19ebe69465bce40
William Elliott <wcelliott1991@gmail.com>
2017-08-10 13:53:44 -0400

    save returns the id

--- test/test-customers-model.js
+++ test/test-customers-model.js
@@ -7,15 +7,13 @@ describe('Customers', () => {
         myCustomer
             .save()
             .then((result) => {
-                console.log('hey there!');
                 done();
             });
     });
     it('should be able to get a customer from the database', (done) => {
-        let myCustomer = new Customer();
-        myCustomer
+        Customer
             .get(1)
-            .then((result) => {
+            .then((myCustomer) => {
                 console.log(myCustomer.name);
                 console.log(myCustomer.email);
                 console.log(myCustomer.address);
@@ -23,4 +21,19 @@ describe('Customers', () => {
                 done();
             });
     })
+    it('should save, provide an id, and then get via id', (done) => {
+        let data = ['Will', 'will@willsworld.com', 'Wills World', "Woohoo!"];
+        let c1 = new Customer(...data);
+        c1.save()
+            .then((result) => {
+                let customer_id = result.customer_id;
+                Customer.get(customer_id)
+                    .then((c2) => {
+                        expect(c2.name).to.equal(data[0]);
+                        expect(c2.email).to.equal(data[1]);
+                        expect(c2.address).to.equal(data[2]);
+                        done();
+                    });
+            });
+    });
 });
