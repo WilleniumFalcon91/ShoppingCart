b401bb987824bb7f83d63c1a2633a5642c40ef85
William Elliott <wcelliott1991@gmail.com>
2017-08-10 14:34:20 -0400

    test currently broken on update db feature

--- customer.js
+++ customer.js
@@ -10,6 +10,18 @@ class Customer {
         this.password = password;
     }
     save() {
+        if (this.customer_id) {
+            return db.one(`
+            update customers
+            set 
+                name=${this.name},
+                email=${this.email},
+                address=${this.address}
+            where customer_id=${this.customer_id}
+            `);
+        } else {
+
+        
         return db.one(`
             insert into customers 
             (name, email, address, password)
@@ -17,6 +29,7 @@ class Customer {
             ('${this.name}', '${this.email}', '${this.address}', '${this.password}')
             returning customer_id;
         `);
+        }
     }
     static get(id) {
         return db.one(`
